05d4dcaa0d98010a2e628c01b64ed9daa1c38ac5
YevheniiaMushyk <142007023+YevheniiaMushyk@users.noreply.github.com>
2024-02-09 15:48:20 +0000

    Deploying to gh-pages from @ YevheniiaMushyk/js_titans@f0c467f41fc7f259e023d22e9835abe43e392ae8 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,2 @@
-import"./assets/styles-6d81ea1e.js";const o=document.querySelector(".favheader__button"),t=document.querySelector(".favmodal__button_close"),e=document.querySelector(".favmodal");o.addEventListener("click",()=>{e.showModal()});t.addEventListener("click",()=>{e.close()});
+import"./assets/styles-1b829a75.js";const o=document.querySelector(".favheader__button"),t=document.querySelector(".favmodal__button_close"),e=document.querySelector(".favmodal");o.addEventListener("click",()=>{e.showModal()});t.addEventListener("click",()=>{e.close()});
 //# sourceMappingURL=commonHelpers.js.map
--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,2 @@
-import"./assets/styles-6d81ea1e.js";const o=document.querySelector(".header__button"),t=document.querySelector(".modal__button_close"),e=document.querySelector(".modal");o.addEventListener("click",()=>{e.showModal()});t.addEventListener("click",()=>{e.close()});
+import"./assets/styles-1b829a75.js";const o=document.querySelector(".header__button"),t=document.querySelector(".modal__button_close"),e=document.querySelector(".modal");o.addEventListener("click",()=>{e.showModal()});t.addEventListener("click",()=>{e.close()});
 //# sourceMappingURL=commonHelpers2.js.map
