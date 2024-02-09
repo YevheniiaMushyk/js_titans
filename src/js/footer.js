67f3e46112423c4e1cbda44e65183c7f9764ac43
Bodernet <142262665+Bodernet@users.noreply.github.com>
2024-02-09 16:32:17 +0200

    JS

---
+++ src/js/footer.js
@@ -0,0 +1,67 @@
+import axios from 'axios';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+// import { showAlert, showLoader } from './......';
+const form = document.querySelector('.footer-form');
+const loader = document.querySelector('.loader');
+
+async function addEmail(email) {
+  return axios.post('https://energyflow.b.goit.study/api/subscription', email);
+}
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+  const userEmail = e.currentTarget.email.value.trim().toLowerCase();
+
+  if (userEmail === '') {
+    showAlert('The email field is empty!', 'ERROR');
+    return;
+  }
+
+  const userData = {
+    email: userEmail,
+  };
+  //   showLoader('true');
+  addEmail(userData)
+    .then(({ data, status }) => {
+      if (status === 201) {
+        showAlert(data.message, 'OK');
+      }
+    })
+    .catch(error => {
+      if (error.response.status === 409) {
+        showAlert('Subscription already exists!');
+      } else {
+        showAlert(error.message, 'ERROR');
+      }
+    })
+    .finally(() => {
+      form.reset();
+      //   showLoader(false);
+    });
+}
+
+export function showAlert(msg, type = 'info') {
+  if (type === 'OK') {
+    iziToast.success({
+      position: 'topCenter',
+      message: msg,
+    });
+  } else if (type === 'ERROR') {
+    iziToast.error({
+      position: 'topCenter',
+      message: msg,
+    });
+  } else {
+    iziToast.info({
+      position: 'topCenter',
+      message: msg,
+    });
+  }
+}
+
+// function showLoader(state = true) {
+//   loader.style.display = !state ? 'none' : 'inline-block';
+//   form.disabled = state;
+// }
