e9ec80e3d7fa80c15f7bd3fd38d3a413c6e7fc4c
YevheniiaMushyk <142007023+YevheniiaMushyk@users.noreply.github.com>
2024-02-10 16:05:57 +0200

    add window position

--- src/js/modal_rating.js
+++ src/js/modal_rating.js
@@ -1,5 +1,5 @@
-const modal_rating_button = document.document.getElementById('modal_rating');
-const modal_rating_close = document.querySelector('.modal__button_close');
+const modal_rating_button = document.getElementById('modal_rating');
+const modal_rating_close = document.querySelector('.modal_rating_close');
 const modal_rating = document.querySelector('.modal_rating');
 
 modal_rating_button.addEventListener('click', () => {
