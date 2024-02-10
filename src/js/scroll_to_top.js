9dbde0fac4aea6667f9c9718269cbe5a3991c083
YelyzavetaMala <malaelizaveta678@gmail.com>
2024-02-10 20:29:15 +0200

    toTop js

--- src/js/scroll_to_top.js
+++ src/js/scroll_to_top.js
@@ -1,16 +1,16 @@
 let toTop = document.getElementById('toTop');
 toTop.style.display = 'none';
 window.addEventListener('scroll', () => {
-    if (this.scrollY > 200) {
+    if (window.scrollY > 200) {
         toTop.style.display = 'block';
     } else {
         toTop.style.display = 'none';
     }
-})
+});
 
 toTop.onclick = function () {
-      window.scrollTo({
-          top: 0,
+    window.scrollTo({
+        top: 0,
         behavior: 'smooth' 
-      });
-}
+    });
+};
