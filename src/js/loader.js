246b20b8dbcbffd33d126ea60bd84b6995d3e291
YevheniiaMushyk <142007023+YevheniiaMushyk@users.noreply.github.com>
2024-02-11 17:08:10 +0200

    adaptive loader

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,9 +1,15 @@
-let mask = document.querySelector(`.mask`);
+// отримуємо доступ до елементу
+const loader = document.getElementById('loader');
 
-window.addEventListener(`load`, () => {
-    mask.classList.add(`hide`);
-    setTimeout(() => {
-        mask.remove();
-    }, 6000)
-})
+//функія для активації loader
+function activeLoader(loader) {
+  loader.classList.remove('loader_hidden');
+}
 
+//функція для деактивації loader
+function disactiveLoader(loader) {
+  loader.classList.add('loader_hidden');
+}
+
+// список експортованих змінних
+export { loader, activeLoader, disactiveLoader };
