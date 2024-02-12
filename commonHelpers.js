845e84c0e8b70122ae4013c96e72c4b96b2e8879
YevheniiaMushyk <142007023+YevheniiaMushyk@users.noreply.github.com>
2024-02-12 11:19:06 +0000

    Deploying to gh-pages from @ YevheniiaMushyk/js_titans@b01d8e66303c0161327e14a2ca0ed2ef72abddb5 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,2 @@
-import"./assets/loader-6f7ca390.js";const o=document.querySelector(".favheader__button"),t=document.querySelector(".favmodal__button_close"),e=document.querySelector(".favmodal");o.addEventListener("click",()=>{e.showModal()});t.addEventListener("click",()=>{e.close()});
+import"./assets/loader-e71f03de.js";const o=document.querySelector(".favheader__button"),t=document.querySelector(".favmodal__button_close"),e=document.querySelector(".favmodal");o.addEventListener("click",()=>{e.showModal()});t.addEventListener("click",()=>{e.close()});
 //# sourceMappingURL=commonHelpers.js.map
