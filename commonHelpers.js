c61010a8970a49282c5c7101f1e5b398bb1d0a9e
YevheniiaMushyk <142007023+YevheniiaMushyk@users.noreply.github.com>
2024-02-14 04:21:04 +0000

    Deploying to gh-pages from @ YevheniiaMushyk/js_titans@f8ca17340a167c85ce37feb735a3ddd79da678b0 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,4 +1,4 @@
-import{o as p}from"./assets/loader-7dd36016.js";import{a as u,i as f}from"./assets/vendor-8cce9181.js";const x=document.querySelector(".favheader__button"),v=document.querySelector(".favmodal__button_close"),l=document.querySelector(".favmodal");x.addEventListener("click",()=>{l.showModal()});v.addEventListener("click",()=>{l.close()});const c={quoteText:document.querySelector(".favorites-text"),quoteAuthor:document.querySelector(".favorites-sub-title")},h="https://energyflow.b.goit.study/api/quote";async function y(e){try{const t=localStorage.getItem("quoteLocalData");if(t){const r=JSON.parse(t),m=n();if(r.date===m){i(r.author,r.quote);return}}const o=(await u.get(e)).data;if(o){const r={quote:o.quote,author:o.author,date:n()};localStorage.setItem("quoteLocalData",JSON.stringify(r)),i(r.author,r.quote)}}catch(t){f.error("Failed to fetch quote data"),console.error(t)}}function n(){const e=new Date,t=String(e.getDate()).padStart(2,"0"),s=String(e.getMonth()+1).padStart(2,"0"),o=e.getFullYear();return`${t}-${s}-${o}`}function i(e,t){c.quoteAuthor.textContent=e,c.quoteText.textContent=t}y(h);const b="https://energyflow.b.goit.study/api/exercises/",_=document.querySelector(".workouts-list");function S(e){return u.get(`${b}/${e}`)}let a=!0;const d=JSON.parse(localStorage.getItem("ENERGY_FLOW_FAVORITES_KEY"))||[];d.length>0?d.forEach(e=>{S(e).then(t=>{const s=t.data;k(s)}).catch(t=>{console.error(t)})}):(a=!1,g());const E=document.querySelectorAll(".exercises-item__button");E.forEach(e=>{e.addEventListener("click",t=>{p(t)})});function k(e){const t=I(e);_.insertAdjacentHTML("beforeend",t),q(e)}function q(e){document.querySelectorAll(".workout-card__remove-btn").forEach(s=>{s.getAttribute("data-workout-id")===e._id&&s.addEventListener("click",()=>{w(s,e._id)})})}function w(e,t){e.closest(".exercises-item").remove();const o=(JSON.parse(localStorage.getItem("ENERGY_FLOW_FAVORITES_KEY"))||[]).filter(r=>r!==t);localStorage.setItem("ENERGY_FLOW_FAVORITES_KEY",JSON.stringify(o)),localStorage.removeItem("workoutId"),o.length===0&&(a=!1),o.length===0&&g()}function I(e){return`
+import"./assets/loader-4b127149.js";import{a as u,i as p}from"./assets/vendor-8cce9181.js";const f=document.querySelector(".favheader__button"),x=document.querySelector(".favmodal__button_close"),l=document.querySelector(".favmodal");f.addEventListener("click",()=>{l.showModal()});x.addEventListener("click",()=>{l.close()});const c={quoteText:document.querySelector(".favorites-text"),quoteAuthor:document.querySelector(".favorites-sub-title")},v="https://energyflow.b.goit.study/api/quote";async function h(e){try{const t=localStorage.getItem("quoteLocalData");if(t){const r=JSON.parse(t),m=n();if(r.date===m){i(r.author,r.quote);return}}const o=(await u.get(e)).data;if(o){const r={quote:o.quote,author:o.author,date:n()};localStorage.setItem("quoteLocalData",JSON.stringify(r)),i(r.author,r.quote)}}catch(t){p.error("Failed to fetch quote data"),console.error(t)}}function n(){const e=new Date,t=String(e.getDate()).padStart(2,"0"),s=String(e.getMonth()+1).padStart(2,"0"),o=e.getFullYear();return`${t}-${s}-${o}`}function i(e,t){c.quoteAuthor.textContent=e,c.quoteText.textContent=t}h(v);const y="https://energyflow.b.goit.study/api/exercises/",g=document.querySelector(".workouts-list");function S(e){return u.get(`${y}/${e}`)}let a=!0;const d=JSON.parse(localStorage.getItem("ENERGY_FLOW_FAVORITES_KEY"))||[];d.length>0?d.forEach(e=>{S(e).then(t=>{const s=t.data;b(s)}).catch(t=>{console.error(t)})}):(a=!1,_());function b(e){console.log(e._id);const t=E(e);g.insertAdjacentHTML("beforeend",t),k(e)}function k(e){document.querySelectorAll(".workout-card__remove-btn").forEach(s=>{s.getAttribute("data-workout-id")===e._id&&s.addEventListener("click",()=>{q(s,e._id)})})}function q(e,t){e.closest(".exercises-item").remove();const o=(JSON.parse(localStorage.getItem("ENERGY_FLOW_FAVORITES_KEY"))||[]).filter(r=>r!==t);localStorage.setItem("ENERGY_FLOW_FAVORITES_KEY",JSON.stringify(o)),localStorage.removeItem("workoutId"),o.length===0&&(a=!1),o.length===0&&_()}function E(e){return`
     <li class="exercises-item">
       <div class="exercises-sub-title">
         <div class="exercises__workout-rating">
@@ -9,8 +9,10 @@ import{o as p}from"./assets/loader-7dd36016.js";import{a as u,i as f}from"./asse
             </svg>
           </button>
         </div>
-     <button class="exercises-item__button" data-workout-id="${e._id}">
+       
+    
   <div class="exercises-start">
+  <button class="exercises-start-button" data-workout-data="${JSON.stringify(e)}">
     <span class="exercises-start__text">Start</span>
     <svg
       class="exercises-start__svg"
@@ -19,9 +21,9 @@ import{o as p}from"./assets/loader-7dd36016.js";import{a as u,i as f}from"./asse
       stroke="rgb(27, 27, 27)"
     >
       <use href="./img/icons.svg#icon-arrow"></use>
-    </svg>
+    </svg></button>
   </div>
-</button>
+
       </div>
       <div class="exercises-title">
         <svg class="exercises-title__svg" width="24" height="24">
@@ -44,7 +46,7 @@ import{o as p}from"./assets/loader-7dd36016.js";import{a as u,i as f}from"./asse
         </p>
       </div>
     </li>
-  `}function g(){a||(_.innerHTML=`
+  `}function _(){a||(g.innerHTML=`
     <div class="empty-list">
       <img class="empty-item"
         srcset="./img/dumbbell@1x-min.png 1x, ./img/dumbbell@1x-min.png 2x"
